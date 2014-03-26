03ff99d66347e233dafabc5f24612f13127c5504
jaredkrinke <jaredkrinke@gmail.com>
2014-03-26 09:09:33 -0700

    Initial test

---
+++ js/main.js
@@ -0,0 +1,7 @@
+﻿// <reference path="http://code.jquery.com/qunit/qunit-1.14.0.js" />
+
+// Just testing this out for now...
+test('first test', function () {
+    ok(true, 'This should pass');
+    ok(false, 'This should fail');
+});
